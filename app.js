107189c005ddfcf740025210cc8f95ddcfac7f08
Shea Hunter <shea.hunter@gmail.com>
2013-05-12 21:18:26 -0500

    redis test

--- app.js
+++ app.js
@@ -2,8 +2,21 @@ var express = require('express');
 var app = express();
 app.use(express.logger());
 
+var redis = require('redis');
+var url = require('url');
+var redisURL = url.parse(process.env.REDISCLOUD_URL);
+var client = redis.createClient(redisURL.port, redisURL.hostname, {no_ready_check: true});
+client.auth(redisURL.auth.split(":")[1]);
+
 app.get('/', function(request, response) {
   response.send('Hello World!');
+});
+
+app.get('/redis', function(request, response) {
+  client.set('foo', 'bar');
+  client.get('foo', function (err, reply) {
+      console.log(reply.toString()); // Will print `bar`
+  });
 });
 
 var port = process.env.PORT || 5000;
